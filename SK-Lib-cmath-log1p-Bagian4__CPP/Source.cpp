630348b3f97fba04f84a71847cfcf4cf14529370
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-27 07:48:50 +0700

    Add project files.

---
+++ SK-Lib-cmath-log1p-Bagian4__CPP/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019 */
+
+using namespace std;
+
+int main() {
+	double x = 21.371, result;
+
+	result = log1p(x);
+	cout << "log1p(x) = " << result << endl;
+
+	_getch();
+	return 0;
+}
